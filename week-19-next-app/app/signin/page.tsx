ce4daaf00f6f9573a86fd42c682f592760db3961
Parva Parmar <parva004cp@gmail.com>
2025-08-04 14:34:13 +0530

    week-19-initial-commit

---
+++ week-19-next-app/app/signin/page.tsx
@@ -0,0 +1,11 @@
+export default function signin() {
+    return (
+        <div>
+            Signin Page
+
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Submit</button>
+        </div>
+    );
+}
