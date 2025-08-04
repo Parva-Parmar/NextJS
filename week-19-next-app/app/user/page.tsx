623579677249cb36cc7f258aab44fd9550bd2a92
Parva Parmar <parva004cp@gmail.com>
2025-08-04 15:16:34 +0530

    blank commit

--- week-19-next-app/app/user/page.tsx
+++ week-19-next-app/app/user/page.tsx
@@ -1,7 +1,17 @@
-export default function User(){
+"use client";
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
+export default async function User(){
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+    const data = response.data;
     return(
         <div>
             User Page
+            <br/>
+            {data.name}
+            <br/>
+            {data.email}
         </div>
     )
 }
