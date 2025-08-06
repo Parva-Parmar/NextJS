52637046349b57cd59243e8d9bbdf7a6d7cd89cb
Parva Parmar <parva004cp@gmail.com>
2025-08-06 18:20:16 +0530

    created signin

--- week-19-next-app/app/page.tsx
+++ week-19-next-app/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      Hi there
+    <div className="text-lg w-screen h-screen flex items-center justify-center">
+      <div>
+        Todo application
+        <br />
+        <Link className="text-md border m-2" href="/signin">Signin to Todo app</Link>
+        <br />
+        <Link className="text-md border m-2" href="/signup">Signup to Todo app</Link>
+      </div>
     </div>
   );
 }
