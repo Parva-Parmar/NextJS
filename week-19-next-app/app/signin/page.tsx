aa221acb45bb16ab624908ac44dfe7259d076518
Parva Parmar <parva004cp@gmail.com>
2025-08-06 19:25:22 +0530

    phase 1

--- week-19-next-app/app/signin/page.tsx
+++ week-19-next-app/app/signin/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
+
 export default function Signin() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     return <div className="w-screen h-screen flex items-center justify-center">
         <div className="border p-2">
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+            <input type="text" placeholder="username" onChange={(e) => setUsername(e.target.value)}></input>
+            <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}></input>
 
             <button onClick={() => {
-
+                axios.post("http://localhost:3000/api/v1/signin",{
+                    username,
+                    password
+                })
             }}>Sign in</button>
         </div>
     </div>
