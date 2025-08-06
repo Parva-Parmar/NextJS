946f00be072bc754004680fe2e430a18efcff0c1
Parva Parmar <parva004cp@gmail.com>
2025-08-06 23:53:04 +0530

    added prisma

--- week-19-next-app/app/api/v1/signup/route.ts
+++ week-19-next-app/app/api/v1/signup/route.ts
@@ -1,9 +1,19 @@
 
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient = new PrismaClient();
 
 export async function POST(req: NextRequest) {
 
     const data = await req.json();
+
+    await prismaClient.user.create({
+        data: {
+            username: data.username,
+            password: data.password
+        }
+    })
 
     console.log(data);
     
@@ -11,3 +21,11 @@ export async function POST(req: NextRequest) {
         message: "You have been signed up"
     })
 }
+
+export async function GET(req: NextRequest) {
+    const user  = await prismaClient.user.findFirst();
+
+    return NextResponse.json({
+        user
+    });
+} 
